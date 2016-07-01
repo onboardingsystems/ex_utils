5d777e84e679f84ee3a294ff02c072239fc5904b
Joseph W. Lindley <josephl@tazworks.com>
2016-07-01 11:31:30 -0600

    Added basic Service skeleton.

--- lib/ex_utils/service.ex
+++ lib/ex_utils/service.ex
@@ -4,7 +4,34 @@ defmodule ExUtils.Service do
     quote do
       use GenServer
       import ExUtils.Service
+      import Ecto.Query
+
+      def start_link(state, opts \\ []) do
+        GenServer.start_link(__MODULE__, state, opts)
+      end
+
+      def handle_call({action, message}, from, state) do
+        response = route_call action, message, from
+        {:reply, response, state}
+      end
+
+      defp route_call(action, message, from) do
+        functions = __MODULE__.__info__(:functions)
+
+        case Keyword.get(functions, action) do
+          0 -> apply(__MODULE__, action, [])
+          1 -> apply(__MODULE__, action, [message])
+          _ -> apply(__MODULE__, action, [message, from])
+        end
+      end
     end
   end
-  
+
+  def call(module, action, message) do
+    GenServer.call module, {action, message}
+  end
+
+  def call(module, action) do
+    GenServer.call module, {action, nil}
+  end
 end
