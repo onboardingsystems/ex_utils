878cd6088893fa19fd1923e6eb37a93c5e23902f
Joseph W. Lindley <josephl@tazworks.com>
2016-07-07 17:19:22 -0600

    something.

--- lib/ex_utils.ex
+++ lib/ex_utils.ex
@@ -1,17 +1,3 @@
 defmodule ExUtils do
-  use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  def start(_type, _args) do
-    import Supervisor.Spec, warn: false
-
-    children = [
-    ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
-    opts = [strategy: :one_for_one, name: ExUtils.Supervisor]
-    Supervisor.start_link(children, opts)
-  end
 end
