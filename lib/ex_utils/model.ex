bf3f82cf37921fe80e4014e29b7c606642d4319d
Joseph W. Lindley <josephl@tazworks.com>
2016-07-07 17:14:04 -0600

    Restored pheonix replacement for schema use.

--- lib/ex_utils/model.ex
+++ lib/ex_utils/model.ex
@@ -1,4 +1,15 @@
 defmodule ExUtils.Model do
+  defmacro __using__(_opts) do
+    quote do
+      use Ecto.Schema
+
+      import Ecto
+      import Ecto.Changeset
+      import Ecto.Query, only: [from: 1, from: 2]
+      import ExUtils.Model
+    end
+  end
+
   @doc """
   Converts a changeset error keyword list to a jsonable map.
   """
