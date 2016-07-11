5f2ad3104bed7e888159d817d4a8a50b3aa53cb7
Joseph W. Lindley <josephl@tazworks.com>
2016-07-11 13:18:40 -0600

    Moved lookup and corrected bug.

--- lib/ex_utils.ex
+++ lib/ex_utils.ex
@@ -18,4 +18,22 @@ defmodule ExUtils do
     opts = [strategy: :one_for_one, name: ExUtil.Supervisor]
     Supervisor.start_link(children, opts)
   end
+
+  def lookup(service_id) do
+    entries = ExUtils.GlobalPresence.list(service_id)
+
+    if entries |> Map.keys |> length > 0 do
+      entries
+      |> Map.keys
+      |> Enum.take_random(1)
+      |> Enum.at(0)
+      |> get_pid(entries)
+    end
+  end
+
+  defp get_pid(key, entries) do
+    entries[key][:metas]
+    |> Enum.at(0)
+    |> Map.get(:pid)
+  end
 end
